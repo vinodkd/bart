10373823d1eba7180a8bfc0e19006c4abb4fe9d0
vinodkd <vinod@vinodkd.org>
2013-02-22 13:10:46 +0530

    first version of the default fixture file

---
+++ bart_fixture.sh
@@ -0,0 +1,15 @@
+#bart_fixture.sh
+
+function setup(){
+	echo "Replace with your setup routine"
+}
+
+function set_testdata(){
+	#sets input test data to blank array.
+	TEST_DATA=( '1 2' '2 3' )
+	#echo "${TEST_DATA[*]} in fixture"
+}
+
+function teardown(){
+	echo "Replace with your teardown routine"
+}
